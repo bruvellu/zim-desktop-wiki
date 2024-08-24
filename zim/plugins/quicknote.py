aeef955a8437cea706316753ae790660befa3bea
Bruno Vellutini <organelas@gmail.com>
2024-08-24 22:02:24 +0200

    Detect any heading level in the first line

--- zim/plugins/quicknote.py
+++ zim/plugins/quicknote.py
@@ -368,10 +368,10 @@ class QuickNoteDialog(Dialog):
 			# Limit page name to contents of first line
 			title = text.split('\n')[0]
 			# Get heading 1 as the page name, if present
-			h1_pattern = r'======\s*(.*?)\s*======'
-			h1_match = re.search(h1_pattern, title)
-			if h1_match:
-				title = h1_match.group(1)
+			heading_pattern = r'={1,}\s*(.*?)\s*={1,}'
+			heading_match = re.search(heading_pattern, title)
+			if heading_match:
+				title = heading_match.group(1)
 			# Remove colons and limit to 50 characters
 			title = title.replace(':', '')[:50]
 			try:
